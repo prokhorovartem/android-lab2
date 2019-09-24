4e799d2114a4aaa874c2b78aa9a40587c276108f
arpr0319 <artem.prokhorov@netcracker.com>
2019-09-24 11:51:20 +0300

    1 task

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+    public static void main(String[] args) {
+        doFirstTask();
+    }
+
+    private static void doFirstTask() {
+        int var1 = 7;
+        int var2 = 10;
+        int result = var1 + var2;
+        System.out.println(result);
+    }
+}
