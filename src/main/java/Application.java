f88ceff5f653686034dfadcdd24926e204c6ceb4
arpr0319 <artem.prokhorov@netcracker.com>
2019-09-24 11:53:15 +0300

    2 task

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 public class Application {
     public static void main(String[] args) {
         doFirstTask();
+        doSecondTask();
     }
 
     private static void doFirstTask() {
@@ -8,5 +9,17 @@ public class Application {
         int var2 = 10;
         int result = var1 + var2;
         System.out.println(result);
+    }
+
+    private static void doSecondTask() {
+        String s1 = "s1";
+        String s2 = "s2";
+        String s3 = "s3";
+        String s4 = "s4";
+        System.out.println(s1 + s2 + s3 + s4);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
     }
 }
